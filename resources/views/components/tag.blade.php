85336d812de5ed57574b735a0543806a38052e8b
Jess Harper <jessica@geniusdivision.com>
2024-11-23 19:33:31 +0000

    build up design

---
+++ resources/views/components/tag.blade.php
@@ -0,0 +1,17 @@
+@props(['size' => 'base'])
+
+@php
+    $classes = "bg-white/10 hover:bg-white/25 py-1 rounded-xl font-bold transition-colors ease-in-out";
+if ($size === 'base') {
+$classes .= ' px-5 text-sm';
+
+}
+
+if ($size === 'small') {
+$classes .= ' px-3 text-2xs';
+}
+
+@endphp
+
+<a href=""
+   class="{{ $classes }}">{{$slot}}</a>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,15 @@ export default {
         './storage/framework/views/*.php',
         './resources/**/*.blade.php',
         './resources/**/*.js',
-        './resources/**/*.vue',
     ],
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                "hanken-grotesk": ["Hanken Grotesk", "sans-serif"]
             },
+            fontSize: {
+                "2xs": ".625rem" // 10px
+            }
         },
     },
     plugins: [],
